e9dd3d8d3a3492599f33a9a1f312dec8e0b646ef
KCDC <kcdcunderscore0@gmail.com>
2026-04-29 23:27:01 +0800

    Day 15

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,19 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (cond ((or (null? s) (null? (cdr s))) #t)
+        ((> (car s) (car (cdr s))) #f)
+        (else (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (cons (car lst2) (interleave lst1 lst2))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+(if (null? s) s
+    (cons (car s)
+      (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
